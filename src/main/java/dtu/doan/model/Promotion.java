21bdd11803462ac4a0957822bb519c02211d0b09
vonguyenphat <vonguyenphat012@gmail.com>
2025-04-01 15:53:10 +0700

    Create Model for project

---
+++ src/main/java/dtu/doan/model/Promotion.java
@@ -0,0 +1,46 @@
+package dtu.doan.model;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import jakarta.persistence.*;
+@Data
+@Entity
+public class Promotion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Column(columnDefinition = ("varchar(255)"))
+    private String title;
+
+    @NotNull
+    @Column(columnDefinition = ("varchar(255)"))
+    private String image;
+
+    @NotNull
+    @Column(columnDefinition = ("text"))
+    private String description;
+
+    @NotNull
+    @Column(columnDefinition = ("varchar(255)"))
+    private String name;
+
+    @Column(columnDefinition = ("varchar(255)"))
+    private String time;
+
+    @NotNull
+    @Column(columnDefinition = ("text"))
+    private String content;
+
+    @NotNull
+    @Column(columnDefinition = ("varchar(255)"))
+    private String location;
+
+    @NotNull
+    @Column(columnDefinition = ("text"), name = "condition_promotion")
+    private String conditionPromotion;
+
+    private Boolean isDelete;
+
+}
