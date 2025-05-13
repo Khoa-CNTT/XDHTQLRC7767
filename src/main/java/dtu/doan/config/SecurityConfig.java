2f2c7a50ec8d8fbdabce2ea34be9784be317d666
PhatVN <vonguyenphat012@gmail.com>
2025-05-13 21:37:24 +0700

    Update BE Genre Phat

--- src/main/java/dtu/doan/config/SecurityConfig.java
+++ src/main/java/dtu/doan/config/SecurityConfig.java
@@ -73,7 +73,8 @@ public class SecurityConfig {
                                 "/api/movies/detail/{id}",
                                 "/api/sentiment/**",
                                 "/auth/social-login",
-                                "/auth/google"
+                                "/auth/google",
+                                "api/tickets/mobile/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
