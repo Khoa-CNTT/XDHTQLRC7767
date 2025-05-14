27b8065e6f15bf08d09f0bba62aba2a04dbb99bc
PhatVN <vonguyenphat012@gmail.com>
2025-05-14 23:49:40 +0700

    Update Customer repository include method findByIsDeleteFalse...

--- src/main/java/dtu/doan/repository/CustomerRepository.java
+++ src/main/java/dtu/doan/repository/CustomerRepository.java
@@ -4,6 +4,7 @@ import dtu.doan.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +16,6 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Customer findByPhoneNumber(String phoneNumber);
 
     Customer findByFullName(String fullName);
+
+    List<Customer> findByIsDeleteFalse();
 }
