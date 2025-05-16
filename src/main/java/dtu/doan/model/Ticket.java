166c68003ee9e8a7a8f47333d84713c56b55aed2
PhatVN <vonguyenphat012@gmail.com>
2025-05-17 01:41:37 +0700

    Update model

--- src/main/java/dtu/doan/model/Ticket.java
+++ src/main/java/dtu/doan/model/Ticket.java
@@ -1,31 +1,16 @@
 package dtu.doan.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import dtu.doan.model.Chair;
-import dtu.doan.model.Customer;
-import dtu.doan.model.Payment;
-import dtu.doan.model.ShowTime;
 import jakarta.persistence.*;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
 
 @Data
 @Entity
 @Table(indexes = {
         @Index(name = "idx_code", columnList = "code"),
-        @Index(name = "idx_status", columnList = "status"),
         @Index(name = "idx_type", columnList = "type"),
         @Index(name = "idx_price", columnList = "price"),
-        @Index(name = "idx_date", columnList = "date"),
-},uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"code", "show_time_id"}),  // Đảm bảo mã vé không trùng
 })
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Ticket {
